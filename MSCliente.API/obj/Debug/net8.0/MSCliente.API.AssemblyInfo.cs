aee3275b7bb8cd6423373206fc827145ee289f61
Lorena <75336847+nalama1@users.noreply.github.com>
2024-12-02 11:26:52 -0500

    primera subida al devOps 02-12

--- MSCliente.API/obj/Debug/net8.0/MSCliente.API.AssemblyInfo.cs
+++ MSCliente.API/obj/Debug/net8.0/MSCliente.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSCliente.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a1bff5aaa23682596a846db1a5e40a704db8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSCliente.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSCliente.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
